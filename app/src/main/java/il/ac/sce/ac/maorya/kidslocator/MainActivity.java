a65a33607777cc748edddabecbcb9a09fc137534
Maor Yatskan <mymaor89@gmail.com>
2020-06-25 15:13:01 +0300

    Initial commit internet, location permission added

---
+++ app/src/main/java/il/ac/sce/ac/maorya/kidslocator/MainActivity.java
@@ -0,0 +1,14 @@
+package il.ac.sce.ac.maorya.kidslocator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
